9977ac386e3cf60d9ef4b7e1d4b3cf66aacc404d
Panga Jayakrishna <pangajayakrishna@Pangas-MacBook-Air.local>
2024-04-13 15:54:39 +0530

    added furl

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
 let express = require("express");
 let cors = require("cors");
 let app = express();
-
 const dotenv = require('dotenv');
 dotenv.config();
-app.use(cors())
-app.use(cors({
-    origin: '*',
-    methods: 'GET, POST, PUT, DELETE, PATCH',
-    allowedHeaders: 'Content-Type',
-  }));
-const bodyParser = require('body-parser');
 
+// Properly configure CORS
+app.use(cors({
+    origin: ['https://rikpalbot-riktam.vercel.app'], // List of domains you want to allow
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true  // if your front end needs to send cookies to the backend
+}));
+
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
 let sendToBackend = require('./src/Router/DBRouter.js');
-
-app.get('/', async (req, res) => {
-    res.send("Server is Running clearly");
-});
-
 app.use('/api', sendToBackend);
 
 const PORT = process.env.PORT || 4000;
