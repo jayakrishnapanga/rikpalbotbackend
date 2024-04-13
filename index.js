6c742c98e5936e778591c8bab6b38d234fcb84a7
Panga Jayakrishna <pangajayakrishna@Pangas-MacBook-Air.local>
2024-04-13 15:13:53 +0530

    added cros

--- index.js
+++ index.js
@@ -1,44 +1,16 @@
-// let express = require("express");
-// let cors = require("cors");
-// let app = express();
-// const dotenv = require('dotenv')
-// dotenv.config();
-// app.use(cors());
-// const bodyParser = require('body-parser');
-
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
-// app.use(express.static("public"))
-// app.use(cors({
-//     origin: '*',
-//     methods: 'GET, POST, PUT, DELETE, PATCH',
-//     allowedHeaders: 'Content-Type',
-// }));
-
-// let sendToBackend = require('./src/Router/DBRouter.js')
-
-// app.get('/', async (req, res) => {
-//     res.send("Server is Running clearly")
-// })
-
-// app.use('/api', sendToBackend)
-
-// app.listen(4000);
-
-
 let express = require("express");
 let cors = require("cors");
 let app = express();
+
 const dotenv = require('dotenv');
 dotenv.config();
-const bodyParser = require('body-parser');
-
-// Configure CORS correctly
+app.use(cors())
 app.use(cors({
-    origin: '*',  // Allows all domains
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
+    origin: '*',
+    methods: 'GET, POST, PUT, DELETE, PATCH',
+    allowedHeaders: 'Content-Type',
+  }));
+const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
